605585bb7c59c74bf49b3cda72eb4e6ab7284eb1
Jeffrey.Kin.So <Kin@sujianfeng.local>
2017-02-07 13:37:22 +0800

    second

--- Practice/AppDelegate.swift
+++ Practice/AppDelegate.swift
@@ -22,7 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         let rootViewController = mainViewController()
         
-        window?.rootViewController = rootViewController
+//        window?.rootViewController = rootViewController
         
         
         
